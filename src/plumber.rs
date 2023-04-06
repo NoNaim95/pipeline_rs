cc25e631fd85983833ff3d29d0e9060e6288bca1
Wappen <lenz.koxholt@gmail.com>
2023-04-06 23:44:35 +0200

    Remove unnecessary PhantomData

--- src/plumber.rs
+++ src/plumber.rs
@@ -1,43 +1,38 @@
 use crate::pipes::transformer_pipe::*;
 use crate::pipes::*;
-use std::marker::PhantomData;
 
-pub struct Plumber<T, P> {
+pub struct Plumber<P> {
     pipe: P,
-    phantom: PhantomData<T>,
 }
 
-impl<T, P> Plumber<T, P> {
+impl<P> Plumber<P> {
     pub fn new(pipe: P) -> Self {
-        Self {
-            pipe,
-            phantom: Default::default(),
-        }
+        Self { pipe }
     }
 }
 
-impl<T, P> From<P> for Plumber<T, P> {
+impl<P> From<P> for Plumber<P> {
     fn from(pipe: P) -> Self {
         Self::new(pipe)
     }
 }
 
 pub trait SendPlumber<T, P> {
-    fn with_transformer<T2, F: FnMut(T2) -> T>(self, t: F) -> Plumber<T2, Transformer<T2, T, F, P>>
+    fn with_transformer<T2, F: FnMut(T2) -> T>(self, t: F) -> Plumber<Transformer<T2, T, F, P>>
     where
         Transformer<T2, T, F, P>: SendPipe<T2>;
     fn complete(self) -> P;
 }
 
 pub trait ReceivePlumber<T, P> {
-    fn with_transformer<T2, F: FnMut(T) -> T2>(self, t: F) -> Plumber<T2, Transformer<T, T2, F, P>>
+    fn with_transformer<T2, F: FnMut(T) -> T2>(self, t: F) -> Plumber<Transformer<T, T2, F, P>>
     where
         Transformer<T, T2, F, P>: ReceivePipe<T2>;
     fn complete(self) -> P;
 }
 
-impl<T, P: SendPipe<T>> SendPlumber<T, P> for Plumber<T, P> {
-    fn with_transformer<T2, F: FnMut(T2) -> T>(self, t: F) -> Plumber<T2, Transformer<T2, T, F, P>>
+impl<T, P: SendPipe<T>> SendPlumber<T, P> for Plumber<P> {
+    fn with_transformer<T2, F: FnMut(T2) -> T>(self, t: F) -> Plumber<Transformer<T2, T, F, P>>
     where
         Transformer<T2, T, F, P>: SendPipe<T2>,
     {
@@ -50,8 +45,8 @@ impl<T, P: SendPipe<T>> SendPlumber<T, P> for Plumber<T, P> {
     }
 }
 
-impl<T, P: ReceivePipe<T>> ReceivePlumber<T, P> for Plumber<T, P> {
-    fn with_transformer<T2, F: FnMut(T) -> T2>(self, t: F) -> Plumber<T2, Transformer<T, T2, F, P>>
+impl<T, P: ReceivePipe<T>> ReceivePlumber<T, P> for Plumber<P> {
+    fn with_transformer<T2, F: FnMut(T) -> T2>(self, t: F) -> Plumber<Transformer<T, T2, F, P>>
     where
         Transformer<T, T2, F, P>: ReceivePipe<T2>,
     {
