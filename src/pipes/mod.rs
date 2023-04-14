1b293fe58fa5d77fb59c18a9a01620252803aef0
Wappen <lenz.koxholt@gmail.com>
2023-04-14 18:54:16 +0200

    Rearrange traits and impls

--- src/pipes/mod.rs
+++ src/pipes/mod.rs
@@ -7,28 +7,8 @@ pub trait ReceivePipe<T>: ReceivePipeMut<T> {
     fn recv(&self) -> T;
 }
 
-pub trait SendPipe<T>: SendPipeMut<T> {
-    fn send(&self, t: T);
-}
-
 pub trait ReceivePipeMut<T> {
     fn recv_mut(&mut self) -> T;
-}
-
-pub trait SendPipeMut<T> {
-    fn send_mut(&mut self, t: T);
-}
-
-impl<T, F: FnMut() -> T> ReceivePipeMut<T> for F {
-    fn recv_mut(&mut self) -> T {
-        (self)()
-    }
-}
-
-impl<T, F: FnMut(T)> SendPipeMut<T> for F {
-    fn send_mut(&mut self, t: T) {
-        (self)(t)
-    }
 }
 
 impl<T, F: Fn() -> T> ReceivePipe<T> for F {
@@ -37,8 +17,28 @@ impl<T, F: Fn() -> T> ReceivePipe<T> for F {
     }
 }
 
+impl<T, F: FnMut() -> T> ReceivePipeMut<T> for F {
+    fn recv_mut(&mut self) -> T {
+        (self)()
+    }
+}
+
+pub trait SendPipe<T>: SendPipeMut<T> {
+    fn send(&self, t: T);
+}
+
+pub trait SendPipeMut<T> {
+    fn send_mut(&mut self, t: T);
+}
+
 impl<T, F: Fn(T)> SendPipe<T> for F {
     fn send(&self, t: T) {
+        (self)(t)
+    }
+}
+
+impl<T, F: FnMut(T)> SendPipeMut<T> for F {
+    fn send_mut(&mut self, t: T) {
         (self)(t)
     }
 }
